e68ef95381fcf4783c808e740b58a4c4e2b7ffc2
kimu_shu <alfvegardrisc@gmail.com>
2017-10-05 10:15:57 +0900

    Update binary

--- software/olive_rubic_duktape/main.c
+++ software/olive_rubic_duktape/main.c
@@ -5,40 +5,19 @@
  *      Author: kimu_shu
  */
 
-#include <unistd.h>
-#include <fcntl.h>
-
-#include "named_fifo.h"
+#include "system.h"
 #include "rubic_agent.h"
-#include "bson.h"
-#include "epcs_fatfs.h"
+//#include "epcs_fatfs.h" // for epcs_fatfs_format()
+
+extern int runtime_duktape_init(void);
+
+int debug_stream_fd = -1;
 
 int main(void)
 {
-	for (;;) {
-		const char *request;
-		const void *params = rubic_agent_wait_request(&request, NULL);
-		int off_target, off_debug, enable_debug;
-		const char *target;
-		
-		if (bson_get_props(params, "target", &off_target, "debug", &off_debug, NULL) < 0) {
-			goto invalid_params;
-		}
-		target = bson_get_string(params, off_target, NULL);
-		if (!target) {
-			goto invalid_params;
-		}
-		enable_debug = bson_get_boolean(params, off_debug, 0);
-
-		if (strcmp(request, "start") == 0) {
-		} else if (strcmp(request, "format") == 0) {
-			epcs_fatfs_format();
-		} else {
-			// unknown request
-		}
-
-invalid_params:
-		rubic_agent_finish_request();
-	}
+	runtime_duktape_init();
+	rubic_agent_register_storage("internal", EPCS_FATFS_MOUNT_POINT);
+	rubic_agent_service();
 	return 0;
 }
+
--- software/olive_rubic_duktape/runtime_duktape.c
+++ software/olive_rubic_duktape/runtime_duktape.c
@@ -3,10 +3,17 @@
  * @brief Runtime invoker for Duktape engine
  */
 
+#include <sys/fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include "sys/alt_dev.h"
+#include "priv/alt_file.h"
 #include "duktape.h"
 #include "dukext.h"
 #include "bson.h"
+#include "rubic_agent.h"
+
+#define AUTO_GC_CYCLES  50
 
 static const char CJS_PROLOGUE[] = "(function(require,module,exports){";
 static const int CJS_PROLOGUE_LEN = sizeof(CJS_PROLOGUE) - 1;
@@ -22,57 +29,82 @@ char debug_push_back_value;
  * @func compile_file
  * @brief Compile JS file to Duktape function object (and push it)
  * @param ctx Duktape context
- * @param filename File name to compile
+ * @param data File name to compile or source data
+ * @param direct Direct mode (data contains source)
  * @return Result of duk_pcompile*()
  */
-static int compile_file(duk_context *ctx, const char *filename)
+static int compile_file(duk_context *ctx, const char *data, int direct)
 {
 	int fd;
 	char *src, *next;
 	duk_size_t file_len, total_len, remainder;
 	duk_int_t result;
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0) {
-		duk_push_error_object(ctx, DUK_ERR_ERROR, "Cannot open file");
-		return -1;
-	}
-	file_len = lseek(fd, 0, SEEK_END);
-	if (file_len == ((duk_size_t)-1)) {
-		duk_push_error_object(ctx, DUK_ERR_ERROR, "Cannot seek file");
-		return -1;
-	}
-	lseek(fd, 0, SEEK_SET);
+    if (direct) {
+        file_len = strlen(data);
+    } else {
+        fd = open(data, O_RDONLY);
+        if (fd < 0) {
+            duk_push_error_object(ctx, DUK_ERR_ERROR, "Cannot open file");
+            return -1;
+        }
+        file_len = lseek(fd, 0, SEEK_END);
+        if (file_len == ((duk_size_t)-1)) {
+            duk_push_error_object(ctx, DUK_ERR_ERROR, "Cannot seek file");
+            return -1;
+        }
+        lseek(fd, 0, SEEK_SET);
+    }
 	total_len = file_len + CJS_PROLOGUE_LEN + CJS_EPILOGUE_LEN;
 	src = malloc(total_len);
 	if (!src) {
+        if (!direct) {
+            close(fd);
+        }
 		duk_push_error_object(ctx, DUK_ERR_ERROR, "Not enough memory");
 		return -1;
 	}
 	next = src;
 	memcpy(next, CJS_PROLOGUE, CJS_PROLOGUE_LEN);
-	next += CJS_PROLOGUE_LEN;
-	for (remainder = file_len; remainder > 0;) {
-		int done = read(fd, next, remainder);
-		if (done < 0) {
-			close(fd);
-			free(src);
-			duk_push_error_object(ctx, DUK_ERR_ERROR, "Cannot read file");
-			return -1;
-		}
-		next += done;
-		remainder -= done;
-	}
-	close(fd);
-	memcpy(next, CJS_EPILOGUE, CJS_EPILOGUE_LEN);
+    next += CJS_PROLOGUE_LEN;
+    if (direct) {
+        memcpy(next, data, file_len);
+        next += file_len;
+    } else {
+        for (remainder = file_len; remainder > 0;) {
+            int done = read(fd, next, remainder);
+            if (done < 0) {
+                close(fd);
+                free(src);
+                duk_push_error_object(ctx, DUK_ERR_ERROR, "Cannot read file");
+                return -1;
+            }
+            next += done;
+            remainder -= done;
+        }
+        close(fd);
+    }
+    memcpy(next, CJS_EPILOGUE, CJS_EPILOGUE_LEN);
 
-	duk_push_string(ctx, filename);
-	result = duk_pcompile_lstring_filename(ctx, 0, src, total_len);
+    duk_push_lstring(ctx, src, total_len);
+    /* [ source ] */
 	free(src);
+    if (direct) {
+        duk_push_string(ctx, "input");
+    } else {
+        duk_push_string(ctx, data);
+    }
+    /* [ source filename ] */
+    result = duk_pcompile(ctx, 0);
+    /* [ function/err ] */
 	return result;
 }
 
-duk_size_t debug_read_cb(void *udata, char *buffer, duk_size_t length)
+/**
+ * @func debug_read_cb
+ * @brief Read callback for Duktape debug stream
+ */
+static duk_size_t debug_read_cb(void *udata, char *buffer, duk_size_t length)
 {
     alt_fd *fd = (alt_fd *)udata;
     int result;
@@ -89,14 +121,22 @@ duk_size_t debug_read_cb(void *udata, char *buffer, duk_size_t length)
     return (result >= 0) ? result : 0;
 }
 
-duk_size_t debug_write_cb(void *udata, const char *buffer, duk_size_t length)
+/**
+ * @func debug_write_cb
+ * @brief Write callback for Duktape debug stream
+ */
+static duk_size_t debug_write_cb(void *udata, const char *buffer, duk_size_t length)
 {
     alt_fd *fd = (alt_fd *)udata;
     int result = (*fd->dev->write)(fd, buffer, length);
     return (result >= 0) ? result : 0;
 }
 
-duk_size_t debug_peek_cb(void *udata)
+/**
+ * @func debug_peek_cb
+ * @brief Peek callback for Duktape debug stream
+ */
+static duk_size_t debug_peek_cb(void *udata)
 {
     alt_fd *fd = (alt_fd *)udata;
     int result;
@@ -112,28 +152,32 @@ duk_size_t debug_peek_cb(void *udata)
 }
 
 /**
- * @file runtime_duktape_run
- * @brief Duktape runner
- * @param request Request object (BSON)
- * @param file File name to run
+ * @file runner
+ * @brief Duktape runner for Rubic agent
  */
-int runtime_duktape_run(const void *request, const char *file)
+static int runner(const char *data, int flags, void *agent_context)
 {
     duk_context *ctx;
-    int off_debug = -1;
-    int enable_debug = 0;
-
-    bson_get_props(request, "debug", &off_debug, NULL);
-    enable_debug = bson_get_boolean(request, off_debug, 0);
+    int result;
+#ifdef AUTO_GC_CYCLES
+    int autoGc = 0;
+#endif
 
     // Initialize Duktape context
     ctx = duk_create_heap_default();
+    if (!ctx) {
+        return -ENOMEM;
+    }
 
     // Initialize duktape-extension
-    dux_initialize(ctx);
+    result = dux_initialize(ctx);
+    if (result != 0) {
+        return result;
+    }
 
     // Attach debugger
-    if (enable_debug) {
+    if ((flags & RUBIC_AGENT_RUNNER_FLAG_DEBUG) && (debug_stream_fd >= 0)) {
+    	alt_fd *fd = &alt_fd_list[debug_stream_fd];
         duk_debugger_attach(ctx,
             debug_read_cb,
             debug_write_cb,
@@ -142,22 +186,47 @@ int runtime_duktape_run(const void *request, const char *file)
             NULL,   // write_flush
             NULL,   // request
             NULL,   // detached
-
+            fd
         );
     }
 
-    // Compile and run
-    if (compile_file(ctx, file) == 0 && duk_pcall(ctx, 0) == 0) {
+    // Compile
+    result = compile_file(ctx, data, (flags & RUBIC_AGENT_RUNNER_FLAG_SOURCE));
+
+    // Notify initialization complete
+    if (rubic_agent_runner_notify_init(agent_context) != 0) {
+        duk_destroy_heap(ctx);
+        return 0;
+    }
+
+    // Run
+    if ((result == 0) && (duk_pcall(ctx, 0) == 0)) {
         // Start tick
         while (dux_tick(ctx)) {
-            const void *intr = rubic_agent_has_interrupt();
-            if (intr) {
+            rubic_agent_runner_cooperate(agent_context);
+            if (rubic_agent_runner_query_abort(agent_context)) {
+                break;
             }
+#ifdef AUTO_GC_CYCLES
+            if (--autoGc < 0) {
+                autoGc = AUTO_GC_CYCLES;
+                duk_gc(ctx, 0);
+            }
+#endif
         }
     } else {
         // Error
         fprintf(stderr, "Error: Compile error\n%s\n", duk_safe_to_string(ctx, -1));
     }
     duk_destroy_heap(ctx);
+    return 0;
+}
 
+/**
+ * @func runtime_duktape_init
+ * @brief Initialize runtime for Duktape
+ */
+int runtime_duktape_init(void)
+{
+    return rubic_agent_register_runtime("duktape", DUK_GIT_DESCRIBE + 1, runner);
 }
