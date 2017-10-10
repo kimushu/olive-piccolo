c8ccd0cc4faa9f8de3cb392778371f3feb2a9db1
kimu_shu <alfvegardrisc@gmail.com>
2017-10-11 00:47:49 +0900

    Update for new Rubic support

--- software/olive_rubic_duktape/main.c
+++ software/olive_rubic_duktape/main.c
@@ -7,16 +7,31 @@
 
 #include "system.h"
 #include "rubic_agent.h"
-//#include "epcs_fatfs.h" // for epcs_fatfs_format()
+#include "peridot_sw_hostbridge_gen2.h"
+#include <unistd.h>
+#include <sys/fcntl.h>
 
 extern int runtime_duktape_init(void);
 
-int debug_stream_fd = -1;
+int debug_stream_fd;
 
 int main(void)
 {
+	// Make pipes for stdio
+	peridot_sw_hostbridge_gen2_mkpipe(2, STDOUT_FILENO, STDIN_FILENO, 1024);
+	peridot_sw_hostbridge_gen2_mkpipe(3, STDERR_FILENO, -1, 0);
+
+	// Make pipe for debug stream
+	debug_stream_fd = open("/dev/null", 0);
+	peridot_sw_hostbridge_gen2_mkpipe(4, debug_stream_fd, debug_stream_fd, 1024);
+
+	// Register runtimes
 	runtime_duktape_init();
+
+	// Register storages
 	rubic_agent_register_storage("internal", EPCS_FATFS_MOUNT_POINT);
+
+	// Start rubic agent
 	rubic_agent_service();
 	return 0;
 }
