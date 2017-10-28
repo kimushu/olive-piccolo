f971ce5f50d3c0dc0c3afeb3968d365f73df2247
kimu_shu <alfvegardrisc@gmail.com>
2017-10-29 01:40:07 +0900

    Rename area name for FPGA config

--- software/olive_rubic_duktape/programmer.c
+++ software/olive_rubic_duktape/programmer.c
@@ -15,7 +15,7 @@ int prog_blocksize(const char *area, void *user_data)
 {
     if (strcmp(area, "spi") == 0) {
         return epcs_fatfs_raw_get_sectsize();
-    } else if (strcmp(area, "img") == 0) {
+    } else if (strcmp(area, "cfg") == 0) {
         return ufm_dev->page_size;
     } else if (strcmp(area, "ufm") == 0) {
         return ufm_dev->page_size;
@@ -28,7 +28,7 @@ int prog_reader(const char *area, void *user_data, int offset, void *ptr, int le
 {
     if (strcmp(area, "spi") == 0) {
         return epcs_fatfs_raw_read(offset, ptr, len);
-    } else if (strcmp(area, "img") == 0) {
+    } else if (strcmp(area, "cfg") == 0) {
     	return alt_onchip_flash_read(&ufm_dev->dev, offset + ufm_dev->sector3_start_addr, ptr, len);
     } else if (strcmp(area, "ufm") == 0) {
     	return alt_onchip_flash_read(&ufm_dev->dev, offset + ufm_dev->sector1_start_addr, ptr, len);
@@ -41,7 +41,7 @@ int prog_writer(const char *area, void *user_data, int offset, const void *ptr, 
 {
     if (strcmp(area, "spi") == 0) {
         return epcs_fatfs_raw_write(offset, ptr, len);
-    } else if (strcmp(area, "img") == 0) {
+    } else if (strcmp(area, "cfg") == 0) {
     	return alt_onchip_flash_write(&ufm_dev->dev, offset + ufm_dev->sector3_start_addr, ptr, len);
     } else if (strcmp(area, "ufm") == 0) {
     	return alt_onchip_flash_write(&ufm_dev->dev, offset + ufm_dev->sector1_start_addr, ptr, len);
