272d1feeab4c6779a0f2d035bb4458a95446c424
kimu_shu <alfvegardrisc@gmail.com>
2017-10-29 01:45:22 +0900

    Rename package

--- software/release.js
+++ software/release.js
@@ -2,14 +2,17 @@ const fs = require("fs");
 const path = require("path");
 const JSZip = require("jszip");
 
+const PACKAGE_JSON = require(path.join(__dirname, "..", "package.json"))
+const FIRM_VERSION = PACKAGE_JSON.version;
 const OUTPUT_FILES = path.join(__dirname, "..", "output_files");
 const RELEASE_JSON = "release.json";
 const info = JSON.parse(fs.readFileSync(path.join(__dirname, RELEASE_JSON)));
 
-const OUTPUT_ZIP = "data.zip";
+const OUTPUT_ZIP = `${PACKAGE_JSON.name}-${FIRM_VERSION}.zip`;
 const RPD_IMG1 = path.join(OUTPUT_FILES, "olive_std_top_cfm1_auto.rpd");
 const RPD_UFM = path.join(OUTPUT_FILES, "olive_std_top_ufm_auto.rpd");
 
+console.info(`Packaging firmware (Version ${FIRM_VERSION})`);
 let zip = new JSZip();
 info.variations.reduce((promise, variation) => {
     let name = variation.path;
@@ -25,7 +28,7 @@ info.variations.reduce((promise, variation) => {
             data = fs.readFileSync(src);
         }
         let zip_in = new JSZip();
-        console.info("Adding " + name + " (" + data.length + " bytes)");
+        console.info("- Adding " + name + " (" + data.length + " bytes)");
         zip_in.file("spi.elf", data);
         zip_in.file("image1.rpd", fs.readFileSync(RPD_IMG1));
         zip_in.file("ufm.rpd", fs.readFileSync(RPD_UFM));
@@ -37,13 +40,13 @@ info.variations.reduce((promise, variation) => {
 }, Promise.resolve())
 .then(() => {
     zip.file(RELEASE_JSON, JSON.stringify(info));
-    console.info("Compressing...");
+    console.info("- Compressing...");
     return zip.generateAsync({
         compression: "DEFLATE",
         type: "nodebuffer"
     });
 })
 .then((content) => {
-    console.info("Writing " + OUTPUT_ZIP + " (" + content.length + " bytes)");
+    console.info("- Writing " + OUTPUT_ZIP + " (" + content.length + " bytes)");
     fs.writeFileSync(OUTPUT_ZIP, content);
 });
